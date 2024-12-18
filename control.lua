068ee225d2500e7917584aeb34d4b96be8996ae6
yaSebastian <17569304+yaSebastian@users.noreply.github.com>
2024-12-19 00:26:24 +0100

    Update project for Factorio mod development
    Add Factorio Modding Tool Kit and set Lua version to 5.2 (used by Factorio)

--- control.lua
+++ control.lua
@@ -1,3 +1,5 @@
+---@diagnostic enable:spell-check
+
 local product_cache = require("utility.product_cache")
 local gui_module = require("utility.gui_module")
 
@@ -12,11 +14,23 @@ end)
 
 -- Events for toggling gui on and off
 script.on_event("toggle_progressive_productivity_gui", function(event)
+    --#region Tell the language server that event_data is of type EventData.CustomInputEvent
+    ---The callback's parameter gets assigned the basic EventData type.
+    ---This is a limitation of @overloads, that has been documented in detail in events.lua
+    ---@cast event EventData.CustomInputEvent
+    --#endregion
+
     local player = game.get_player(event.player_index)
     gui_module.toggleProgressiveProductivityUI(player, event.tick)
 end)
 
 script.on_event(defines.events.on_lua_shortcut, function(event)
+    --#region Tell the language server that event_data is of type EventData.on_lua_shortcut
+    ---The callback's parameter gets assigned the basic EventData type.
+    ---This is a limitation of @overloads, that has been documented in detail in events.lua
+    ---@cast event EventData.on_lua_shortcut
+    --#endregion
+
     if event.prototype_name == "toggle_progressive_productivity_gui_shortcut" then
         local player = game.get_player(event.player_index)
         gui_module.toggleProgressiveProductivityUI(player, event.tick)
@@ -24,6 +38,12 @@ script.on_event(defines.events.on_lua_shortcut, function(event)
 end)
 
 script.on_event(defines.events.on_gui_closed, function(event)
+    --#region Tell the language server that event_data is of type EventData.on_gui_closed
+    ---The callback's parameter gets assigned the basic EventData type.
+    ---This is a limitation of @overloads, that has been documented in detail in events.lua
+    ---@cast event EventData.on_gui_closed
+    --#endregion
+
     if event.element and event.element.name == "progressive_productivty_list" then
         local player = game.get_player(event.player_index)
         gui_module.toggleProgressiveProductivityUI(player, event.tick)
