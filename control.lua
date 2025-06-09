81ec446a4291fa382eb2dfdd911986d308c6d3d9
Joseph Quesnel <76413242+josephquesnel@users.noreply.github.com>
2025-06-09 05:30:45 -0400

    Update control.lua
    added migration flag (not working well yet)

--- control.lua
+++ control.lua
@@ -1,12 +1,28 @@
+-- Ensure global exists
+if global == nil then global = {} end
+if global.storage == nil then global.storage = {} end
+
 local product_cache = require("utility.product_cache")
 local gui_module = require("utility.gui_module")
 
--- When recipes could have changed or when we are initialized, create our local storage
-script.on_configuration_changed(function(event)
+script.on_init(function()
+    global.storage.items = {}
+    global.storage.productivityPercents = {}
     product_cache.setupStorage()
 end)
 
-script.on_init(function(event)
+-- On loading a saved game
+script.on_load(function()
+    if global.storage.items == nil then global.storage.items = {} end
+    if global.storage.productivityPercents == nil then global.storage.productivityPercents = {} end
+
+    if next(global.storage.productivityPercents) == nil then
+        global.storage.migration_needed = true
+    end
+end)
+
+-- When recipes could have changed or when we are initialized, create our local storage
+script.on_configuration_changed(function(event)
     product_cache.setupStorage()
 end)
 
