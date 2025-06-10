3a641173be4951eda312d2d60f2b3e6225f4acf2
Joseph Quesnel <76413242+josephquesnel@users.noreply.github.com>
2025-06-09 23:51:19 -0400

    Update control.lua
    reverted control lua, fixed all issues.

--- control.lua
+++ control.lua
@@ -1,29 +1,18 @@
 -- Ensure global exists
-if global == nil then global = {} end
-if global.storage == nil then global.storage = {} end
+if storage == nil then storage = {} end
 
 local product_cache = require("utility.product_cache")
 local gui_module = require("utility.gui_module")
 
 script.on_init(function()
-    global.storage.items = {}
-    global.storage.productivityPercents = {}
-    product_cache.setupStorage()
-end)
-
--- On loading a saved game
-script.on_load(function()
-    if global.storage.items == nil then global.storage.items = {} end
-    if global.storage.productivityPercents == nil then global.storage.productivityPercents = {} end
-
-    if next(global.storage.productivityPercents) == nil then
-        global.storage.migration_needed = true
-    end
+    	storage.items = {}
+    	storage.productivityPercents = {}
+    	product_cache.setupStorage()
 end)
 
 -- When recipes could have changed or when we are initialized, create our local storage
 script.on_configuration_changed(function(event)
-    product_cache.setupStorage()
+   	product_cache.setupStorage()
 end)
 
 -- Events for toggling gui on and off
