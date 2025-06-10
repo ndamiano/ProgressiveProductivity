ff09b01ba28bc95aada784d0162e4d74d3c2d2ca
Joseph Quesnel <76413242+josephquesnel@users.noreply.github.com>
2025-06-10 00:13:38 -0400

    Update control.lua
    more formatting

--- control.lua
+++ control.lua
@@ -3,30 +3,30 @@ local gui_module = require("utility.gui_module")
 
 -- When recipes could have changed or when we are initialized, create our local storage
 script.on_configuration_changed(function(event)
-   	product_cache.setupStorage()
+   product_cache.setupStorage()
 end)
 
 script.on_init(function()
-    storage.items = {}
-    product_cache.setupStorage()
+   storage.items = {}
+   product_cache.setupStorage()
 end)
 
 -- Events for toggling gui on and off
 script.on_event("toggle_progressive_productivity_gui", function(event)
-    local player = game.get_player(event.player_index)
-    gui_module.toggleProgressiveProductivityUI(player, event.tick)
+   local player = game.get_player(event.player_index)
+   gui_module.toggleProgressiveProductivityUI(player, event.tick)
 end)
 
 script.on_event(defines.events.on_lua_shortcut, function(event)
-    if event.prototype_name == "toggle_progressive_productivity_gui_shortcut" then
-        local player = game.get_player(event.player_index)
-        gui_module.toggleProgressiveProductivityUI(player, event.tick)
-    end
+   if event.prototype_name == "toggle_progressive_productivity_gui_shortcut" then
+      local player = game.get_player(event.player_index)
+      gui_module.toggleProgressiveProductivityUI(player, event.tick)
+   end
 end)
 
 script.on_event(defines.events.on_gui_closed, function(event)
-    if event.element and event.element.name == "progressive_productivty_list" then
-        local player = game.get_player(event.player_index)
-        gui_module.toggleProgressiveProductivityUI(player, event.tick)
-    end
+   if event.element and event.element.name == "progressive_productivty_list" then
+      local player = game.get_player(event.player_index)
+      gui_module.toggleProgressiveProductivityUI(player, event.tick)
+   end
 end)
